bb5628ea03b327817da70753f38e620e4124577c
Alejo Feas Matej <alefeas99@gmail.com>
2025-10-30 02:41:32 -0300

    Fix CORS to allow all origins

--- config/cors.php
+++ config/cors.php
@@ -4,12 +4,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [
-        '/^https:\/\/.*\.vercel\.app$/',
-        '/^http:\/\/localhost(:\d+)?$/'
-    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
